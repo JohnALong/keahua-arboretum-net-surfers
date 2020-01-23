8a3f7fc903ff211b60b1326c57b5c8b1823dc4df
Trey Suiter <trey.suiter@gmail.com>
2020-01-23 14:27:48 -0600

    formatting

--- plants/plant.py
+++ plants/plant.py
@@ -1,12 +1,18 @@
 from interfaces import identifiable
 
+
 class Plant:
 
-    def __init__(self, species, season):
+    '''
+    Attributes for Plant (all string unless noted):
+    species, sunlight, seed_produced (int), insecticide_resistance, season
+    '''
+
+    def __init__(self, species, sunlight, seeds_produced, insecticide_resistance, season):
 
         Identifiable.__init__(self)
         self.species = species
-        self.sunlight = ""
-        self.seeds_produced = 0
-        self.insecticide_resistance = ""
+        self.sunlight = sunlight
+        self.seeds_produced = seeds_produced
+        self.insecticide_resistance = insecticide_resistance
         self.peak_season = season
