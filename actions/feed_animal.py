84788775b2d9096d486cd5626e4ed59bf13dc845
John Long <jlcomp45@gmail.com>
2020-01-24 15:34:06 -0600

    commit to check pull request

---
+++ actions/feed_animal.py
@@ -0,0 +1,12 @@
+import os
+
+from animals import RiverDolphin
+from animals import GoldDustDayGecko
+from animals import HawaiianHappyfaceSpider
+from animals import NeneGoose
+from animals import Opeapea
+from animals import Ulae
+from animals import Kikakapu
+from animals import Pueo
+
+def release_animal(arboretum):
