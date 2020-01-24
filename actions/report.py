2e0476946ecfa6e719287434cb7469918929893f
Sullivan Pierce <sullypierce@gmail.com>
2020-01-24 11:18:14 -0600

    finishing report

--- actions/report.py
+++ actions/report.py
@@ -3,7 +3,10 @@ def build_facility_report(arboretum):
     for biome_list in arboretum.biomes:
         print(biome_list,":")
         for biome in arboretum.biomes[biome_list]:
-            print (biome)
+            print (biome, ":")
+            print("")
+            for animal in biome:
+                
  
     input("\n\nPress any key to continue...")
  
