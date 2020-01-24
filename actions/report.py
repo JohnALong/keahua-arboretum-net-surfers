570b1a65477e81f03619695446111d70f189f173
Sullivan Pierce <sullypierce@gmail.com>
2020-01-24 15:44:43 -0600

    g

--- actions/report.py
+++ actions/report.py
@@ -10,18 +10,18 @@ def build_facility_report(arboretum):
             print(biome_list,":")
             #loop through a list of biomes of a distinct type
             for biome in arboretum.biomes[biome_list]:
-                print (biome, f"Id: [{str(biome.id)[:9]}]")
+                print (biome, f"Id: [{str(biome.id)[:8]}]")
                 #check if there are any animals in a specific biome
                 if len(biome.animals) > 0:
                     print("Animals: ")
                     #loop through an individual biome's animals
                     for animal in biome.animals:
-                        print(animal.species, f"Id: ({str(animal.id)[:9]})")
+                        print(animal.species, f"Id: ({str(animal.id)[:8]})")
                 #check if there are any plants in a specific biome
                 if len(biome.plants) > 0:
                     print("Plants: ")
                     for plant in biome.plants:
-                        print(plant.species, f"Id: ({str(plant.id)[:9]})")
+                        print(plant.species, f"Id: ({str(plant.id)[:8]})")
                 print("-----------")
             print("-------------------------------")
  
