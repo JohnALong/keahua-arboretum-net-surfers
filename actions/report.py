d76edac12b0275555b2f7f24be8de0ff898c391b
Sullivan Pierce <sullypierce@gmail.com>
2020-01-24 11:32:32 -0600

    almost finished

--- actions/report.py
+++ actions/report.py
@@ -3,10 +3,14 @@ def build_facility_report(arboretum):
     for biome_list in arboretum.biomes:
         print(biome_list,":")
         for biome in arboretum.biomes[biome_list]:
-            print (biome, ":")
-            print("")
+            print (biome, "Id: {}")
+            print("Animals: ")
             for animal in biome.animals:
                 print(animal.name, f"Id: {animal.id}")
+            print("Plants: ")
+            for plant in biome.plants:
+                print(plant.name, f"Id: {plant.id}")
+            print("-----------------")
  
     input("\n\nPress any key to continue...")
  
