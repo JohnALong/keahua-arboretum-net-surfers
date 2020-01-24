9ab602a6e51133acc1a0dcb3b681d7bbf4b161b1
Sullivan Pierce <sullypierce@gmail.com>
2020-01-24 14:04:22 -0600

    added if statements to only show whats there

--- actions/report.py
+++ actions/report.py
@@ -1,15 +1,24 @@
 
 def build_facility_report(arboretum):
+    #loop through the different types of biomes
     for biome_list in arboretum.biomes:
-        print(biome_list,":")
-        for biome in arboretum.biomes[biome_list]:
-            print (biome, "Id: {}")
-            print("Animals: ")
-            for animal in biome.animals:
-                print(animal.name, f"Id: {animal.id}")
-            print("Plants: ")
-            for plant in biome.plants:
-                print(plant.name, f"Id: {plant.id}")
+        #check whether each type of biome has any actually biomes in the arboretum
+        if len(arboretum.biomes[biome_list]) > 0:
+            print(biome_list,":")
+            #loop through a list of biomes of a distinct type
+            for biome in arboretum.biomes[biome_list]:
+                print (biome, "Id: {}")
+                #check if there are any animals in a specific biome
+                if len(biome.animals) > 0:
+                    print("Animals: ")
+                    #loop through an individual biome's animals
+                    for animal in biome.animals:
+                        print(animal.name, f"Id: {animal.id}")
+                #check if there are any plants in a specific biome
+                if len(biome.plants) > 0:
+                    print("Plants: ")
+                    for plant in biome.plants:
+                        print(plant.name, f"Id: {plant.id}")
             print("-----------------")
  
     input("\n\nPress any key to continue...")
