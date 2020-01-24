1d0373ffb6734851799aacd3f1ed663e286e9b7c
John Long <jlcomp45@gmail.com>
2020-01-24 09:30:23 -0600

    commit to check pull request

--- arboretum.py
+++ arboretum.py
@@ -4,3 +4,7 @@ class Arboretum:
         self.address = address
         self.rivers = []
         self.grasslands = []
+        self.mountains = []
+        self.coastline = []
+        self.forest = []
+        self.swamp = []
--- environments/__init__.py
+++ environments/__init__.py
@@ -4,3 +4,4 @@ from .mountain import Mountain
 from .swamp import Swamp
 from .grassland import Grassland
 from .forest import Forest
+from .coastline import Coastline
