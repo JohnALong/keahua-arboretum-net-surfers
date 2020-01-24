bbed64618e86f77f0edc192cef07790c36240663
Sullivan Pierce <sullypierce@gmail.com>
2020-01-24 11:50:12 -0600

    added interface inheritance to animals

--- animals/nene_goose.py
+++ animals/nene_goose.py
@@ -1,5 +1,7 @@
 from animals import Animal
+from interfaces import IGrassland
 
-class NeneGoose(Animal):
+class NeneGoose(Animal, IGrassland):
     def __init__(self):
         Animal.__init__(self, "Nene Goose")
+        IGrassland.__init__(self)
