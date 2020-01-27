1499e10afa3ed2f851e9a1e23ef472a9f2f0aacc
Sullivan Pierce <sullypierce@gmail.com>
2020-01-27 10:34:41 -0600

    in progress

--- actions/report.py
+++ actions/report.py
@@ -1,5 +1,5 @@
 
-def build_facility_report(arboretum):
+def build_full_report(arboretum):
     print("Keahua Arboretum:")
     print("-----------------")
     print("-----------------")
@@ -24,6 +24,34 @@ def build_facility_report(arboretum):
                         print(plant.species, f"Id: ({str(plant.id)[:8]})")
                 print("-----------")
             print("-------------------------------")
- 
+
     input("\n\nPress any key to continue...")
- 
+
+def build_single_report(arboretum):
+    index_dict = dict()
+    for biome_type in arboretum.biomes:
+        if len(arboretum.biomes[biome_type]) > 0:
+            print(f"({biome_type[:1]}) {biome_type}")
+            index_plus_one = 1
+            for biome in arboretum.biomes[biome_type]:
+                print(f"{index_plus_one}. {biome}")
+                index_dict[f"{biome_type[:1]-{index_plus}}"] = [biome_type, biome]
+    choice = input("Enter the letter and number corresponding to your choice.")
+
+
+def build_facility_report(arboretum):
+
+    print("Would you like a full report, or a single biome report?")
+    print("1. Full")
+    print("2. Single")
+
+    choice = input(" >>  ")
+
+    if choice == "1":
+        build_full_report(arboretum)
+    
+    elif choice == "2":
+        build_single_report(arboretum)
+
+    else:
+        build_facility_report(arboretum)
