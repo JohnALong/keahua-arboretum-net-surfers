e9fba0fed1bf77829ef621025dcdb16d1a153b16
Sullivan Pierce <sullypierce@gmail.com>
2020-01-27 13:27:56 -0600

    done

--- environments/forest.py
+++ environments/forest.py
@@ -2,5 +2,5 @@ from environments import Biome
 
 class Forest(Biome):
     def __init__(self, name):
-        Biome.__init__(self, f"{name} Forest")
+        Biome.__init__(self, f"{name} Forest", 32, 20)
       
--- environments/mountain.py
+++ environments/mountain.py
@@ -2,4 +2,4 @@ from environments import Biome
 
 class Mountain(Biome):
     def __init__(self, name):
-        Biome.__init__(self, f"{name} Mountain")
+        Biome.__init__(self, f"{name} Mountain", 4, 6)
