9ac33e0b830b40bce470ac46c7f7c1772a1209e1
Ryan Bishop <rdbishop06@gmail.com>
2020-01-28 09:22:33 -0600

    fixed hawaiian spellings on ulae and kikakapu

--- animals/ulae.py
+++ animals/ulae.py
@@ -4,6 +4,6 @@ from interfaces import IEatFish
 
 class Ulae(Animal, ICoastline, IEatFish):
     def __init__(self):
-        Animal.__init__(self, "'Ulae'")
+        Animal.__init__(self, "'Ulae")
         ICoastline.__init__(self)
         IEatFish.__init__(self)
