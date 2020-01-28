3778bbddb877094bcc8bdc4e13560129bea4aeb5
Ryan Bishop <rdbishop06@gmail.com>
2020-01-28 15:20:52 -0600

    refactor: build_biome_menu

--- actions/release_animal.py
+++ actions/release_animal.py
@@ -44,6 +44,23 @@ def build_menu():
     print('\nEnter "Q" to return to Main Menu.')
     return
 
+@menu_wrapper
+def build_biome_menu(arboretum, biome_dict, biome_type):
+
+    # targets specific previously created biome list in arboretum
+        # dictionary to store the user choice options ONLY if they have space remaining
+    actual_count = 0 # container to hold the count
+    for index, biome in enumerate(arboretum.biomes[biome_type]):
+        num_current_animals = len(biome.animals)
+        if num_current_animals == biome.max_animals: # skip this biome if it's already FULL
+            continue
+        else:             # add this BIOME to the available choices in biome_dict
+            actual_count += 1  # this helps avoid a menu starting at '2' if a previous one was skipped
+            biome_dict[actual_count - 1] = biome  # this still saves the biome to a zero-indexed dict
+            print(f'{actual_count}. {biome.name} [{num_current_animals} animal(s), {biome.max_animals - num_current_animals} remaining capacity]')
+
+    return biome_dict
+
 def release_animal(arboretum):
     clear_screen()
 
@@ -118,20 +135,11 @@ def release_animal(arboretum):
     # if valid choice, clear screen and proceed with selecting a biome
     clear_screen()
 
-    # targets specific previously created biome list in arboretum
-    biome_dict = dict() # dictionary to store the user choice options ONLY if they have space remaining
-    actual_count = 0 # container to hold the count
-    for index, biome in enumerate(arboretum.biomes[biome_type]):
-        num_current_animals = len(biome.animals)
-        if num_current_animals == biome.max_animals: # skip this biome if it's already FULL
-            continue
-        else:             # add this BIOME to the available choices in biome_dict
-            actual_count += 1  # this helps avoid a menu starting at '2' if a previous one was skipped
-            biome_dict[actual_count - 1] = biome  # this still saves the biome to a zero-indexed dict
-            print(f'{actual_count}. {biome.name} [{num_current_animals} animal(s), {biome.max_animals - num_current_animals} remaining capacity]')
-
-    # if ALL biomes of this type full, return to main menu after notification
-    if actual_count == 0:
+    biome_dict = dict()
+    build_biome_menu(arboretum, biome_dict, biome_type)
+    # print(biome_dict)
+     # if ALL biomes of this type full, return to main menu after notification
+    if not biome_dict:
         input(f'All biomes of this type are at max animal capacity.\nAnnex a new {biome_type[:-1]} habitat from the main menu.')
         return
 
@@ -140,7 +148,7 @@ def release_animal(arboretum):
 
     try:
         new_home = biome_dict[int(choice) - 1] # this gets the input choice and converts back to zero-indexed choice
-    except (KeyError, ValueError):
+    except (KeyError, ValueError, TypeError):
         input("Invalid input. Return to main menu.")
         # release_animal(arboretum)
         return
